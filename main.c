3cf95ac833a424899ff3646ad0c5437fe0ed485a
kartikeyaswarup-m1 <swarupkartikeya@gmail.com>
2025-05-21 11:11:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 extern int yyparse();
+extern void printIC();
+extern void printAssembly();
 extern FILE *yyin;
-void finalizeCompilation();  
 
-int main() {
-    yyin = stdin;
-    yyparse();
-    finalizeCompilation(); 
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        yyin = fopen(argv[1], "r");
+        if (!yyin) {
+            perror("File open error");
+            return 1;
+        }
+    }
+
+    int parseResult = yyparse();
+
+    if (parseResult == 0) {
+        printIC();
+        printAssembly();
+    } else {
+        fprintf(stderr, "Parsing failed.\n");
+    }
+
     return 0;
 }
